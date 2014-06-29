3e86e69f2ef77296467e39c2b70ad5352ba0a1ac
Joshua Scoggins <Theoretically.x64@gmail.com>
2014-06-28 19:26:37 -0700

    Added an operation lookup table

--- arithmetic.go
+++ arithmetic.go
@@ -2,3 +2,19 @@
 package edgeworth
 
 type ArithmeticOperation func(Word, Word) Word
+
+var ArithmeticOperations = []ArithmeticOperation{
+	func(x, _ Word) Word { return x },
+	func(_, y Word) Word { return y },
+	func(x, y Word) Word { return x + y },
+	func(x, y Word) Word { return x - y },
+	func(x, y Word) Word { return x * y },
+	func(x, y Word) Word { return x / y },
+	func(x, y Word) Word { return x % y },
+	func(x, y Word) Word { return x << y },
+	func(x, y Word) Word { return x >> y },
+	func(x, y Word) Word { return x & y },
+	func(x, y Word) Word { return x | y },
+	func(x, _ Word) Word { return ^x },
+	func(x, y Word) Word { return x ^ y },
+}
