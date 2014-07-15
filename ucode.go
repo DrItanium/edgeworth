94e781f6a5e3f3ed1e726486772c059794dcb248
Joshua Scoggins <Theoretically.x64@gmail.com>
2014-07-14 21:55:15 -0700

    Added conditional operations

--- ucode.go
+++ ucode.go
@@ -1,10 +1,19 @@
 // different microcode operations
 package edgeworth
 
+type ConditionalOperation func(Word, Word) bool
 type BinaryOperation func(Word, Word) Word
 type TrinaryOperation func(Word, Word, Word) Word
 
-var arithmeticOperations = []BinaryOperation{
+func BitToWord(val Bit) Word {
+	if val {
+		return 1
+	} else {
+		return 0
+	}
+}
+
+var Operations = []BinaryOperation{
 	func(x, y Word) Word { return x + y },
 	func(x, y Word) Word { return x - y },
 	func(x, y Word) Word { return x * y },
@@ -16,5 +25,10 @@ var arithmeticOperations = []BinaryOperation{
 	func(x, y Word) Word { return x | y },
 	func(x, y Word) Word { return x &^ y },
 	func(x, _ Word) Word { return ^x },
-	func(x, y Word) Word { return Word(x == y) },
+	func(x, y Word) Word { return BitToWord(x == y) },
+	func(x, y Word) Word { return BitToWord(x != y) },
+	func(x, y Word) Word { return BitToWord(x > y) },
+	func(x, y Word) Word { return BitToWord(x < y) },
+	func(x, y Word) Word { return BitToWord(x >= y) },
+	func(x, y Word) Word { return BitToWord(x <= y) },
 }
