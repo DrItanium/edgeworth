b87db261ccb3ce152747f247cfc97fe5a061a01e
Joshua Scoggins <Theoretically.x64@gmail.com>
2014-07-16 19:14:47 -0700

    Edgeworth is iris implemented in go

--- edgeworth.go
+++ edgeworth.go
@@ -1,41 +1,19 @@
-// a 32-bit architecture with peculiarities which make it a perfect target as a microcode language
-// It is based off of the older 16-bit iris architecture
+// An implementation of iris in go
 package edgeworth
+
+import "fmt"
 
 type Bit bool
 type RegisterIndex byte
-type ControlBits uint16
-type GroupBits byte // four bits logically
-type OpBits uint16  // twelve bits logically
-type Instruction uint64
-
-/*
-taken from phoenix
-{
-	control uint16
-	destination0 byte
-	destination1 byte
-	source0 byte
-	source1 byte
-	source2 byte
-	source3 byte
-}
-
-and
-
-{
-	control uint16
-	destination0 byte
-	destination1 byte
-	immediate Word
-}
-
-*/
-type Word uint32
+type GroupBits byte
+type OpBits byte
+type ControlBits byte
+type Instruction uint32
+type Word uint16
 
 const (
 	RegisterCount       = 256
-	MemorySize          = 16777216
+	MemorySize          = 65536
 	InstructionGroupMax = 8
 )
 
@@ -48,85 +26,62 @@ type Core struct {
 	AdvanceIP          Bit
 	TerminateExecution Bit
 }
+type InstructionRegisterRequestError struct {
+	Write bool
+	Index int
+}
 
+func (e *InstructionRegisterRequestError) Error() string {
+	if e.Write {
+		return fmt.Printf("Attempted to write to register field %d which is out of range", e.Index)
+	} else {
+		return fmt.Printf("Attempted to request register field %d which is out of range", e.Index)
+	}
+}
 func (i *Instruction) GetControlBits() ControlBits {
-	return ControlBits(*i & 0x000000000000FFFF)
+	return ControlBits(*i & 0x000000FF)
+}
+func (i *Instruction) GetRegister(index int) (RegisterIndex, error) {
+	switch index {
+	case 0:
+		return RegisterIndex(*i & 0x0000FF00 >> 8), nil
+	case 1:
+		return RegisterIndex(*i & 0x00FF0000 >> 16), nil
+	case 2:
+		return RegisterIndex(*i & 0xFF000000 >> 24), nil
+	default:
+		return 0, InstructionRegisterRequestError{Index: index, Write: false}
+	}
 }
 
-func (i *Instruction) GetDestination0() RegisterIndex {
-	return RegisterIndex(*i & 0x0000000000FF0000 >> 16)
+func (i *ControlBits) GetGroup() GroupBits {
+	return GroupBits(*i & 0x7)
 }
-func (i *Instruction) GetDestination1() RegisterIndex {
-	return RegisterIndex(*i & 0x00000000FF000000 >> 24)
+
+func (i *ControlBits) GetOperation() OpBits {
+	return OpBits(*i & 0xF8 >> 3)
 }
 
 func (i *Instruction) GetImmediate() Word {
-	return Word(*i & 0xFFFFFFFF00000000 >> 32)
+	return Word(*i & 0xFFFF0000 >> 16)
 }
 
-func (i *Instruction) GetSource0() RegisterIndex {
-	return RegisterIndex(*i & 0x000000FF00000000 >> 32)
+func (i *Instruction) SetImmediate(value Word) {
+	*i = *i&^0xFFFF0000 | Instruction(v)<<16
 }
 
-func (i *Instruction) GetSource1() RegisterIndex {
-	return RegisterIndex(*i & 0x0000FF0000000000 >> 40)
-}
-
-func (i *Instruction) GetSource2() RegisterIndex {
-	return RegisterIndex(*i & 0x00FF000000000000 >> 48)
-}
-
-func (i *Instruction) GetSource3() RegisterIndex {
-	return RegisterIndex(*i & 0xFF00000000000000 >> 56)
-}
-
-func (i *Instruction) SetControlBits(bits ControlBits) {
-	*i = *i&^0x000000000000FFFF | Instruction(bits)
-}
-
-func (i *Instruction) SetDestination0(rd RegisterIndex) {
-	*i = *i&^0x0000000000FF0000 | Instruction(rd)<<16
-}
-
-func (i *Instruction) SetDestination1(rd RegisterIndex) {
-	*i = *i&^0x00000000FF000000 | Instruction(rd)<<24
-}
-func (i *Instruction) SetSource0(rs RegisterIndex) {
-	*i = *i&^0x000000FF00000000 | Instruction(rs)<<32
-}
-func (i *Instruction) SetSource1(rs RegisterIndex) {
-	*i = *i&^0x0000FF0000000000 | Instruction(rs)<<40
-}
-func (i *Instruction) SetSource2(rs RegisterIndex) {
-	*i = *i&^0x00FF000000000000 | Instruction(rs)<<48
-}
-
-func (i *Instruction) SetSource3(rs RegisterIndex) {
-	*i = *i&^0xFF00000000000000 | Instruction(rs)<<56
-}
-
-func (i *Instruction) SetImmediate(imm Word) {
-	*i = *i&^0xFFFFFFFF00000000 | Instruction(imm)<<32
-}
-
-func (i *ControlBits) GetGroupBits() GroupBits {
-	// we can have a maximum of 16 different groups
-	return GroupBits(*i & 0x000F)
-}
-
-func (i *ControlBits) GetOpBits() OpBits {
-	// we can have a maximum of 4096 operations in a group
-	return OpBits(*i & 0xFFF0 >> 4)
-}
-
-func (i *ControlBits) SetOpBits(a OpBits) {
-	// clear out the upper 4 bits of the opbits
-	fix := a & 0x0FFF
-	*i = *i&^0xFFF0 | ControlBits(fix<<4)
-}
-
-func (i *ControlBits) SetGroupBits(a GroupBits) {
-	// clear out the upper 4 bits of the group bits
-	fix := a & 0x0F
-	*i = *i&^0x000F | ControlBits(fix)
+func (i *Instruction) SetRegister(index int, value RegisterIndex) error {
+	switch index {
+	case 0:
+		*i = *i&^0x0000FF00 | Instruction(value)<<8
+		return nil
+	case 1:
+		*i = *i&^0x00FF0000 | Instruction(value)<<16
+		return nil
+	case 2:
+		*i = *i&^0xFF000000 | Instruction(value)<<24
+		return nil
+	default:
+		return InstructionRegisterRequestError{Index: index, Write: true}
+	}
 }
