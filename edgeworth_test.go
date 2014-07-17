9885411f4a2ff4a76ecf8aaa50da0119ef6863b4
Joshua Scoggins <Theoretically.x64@gmail.com>
2014-07-16 20:12:00 -0700

    Updated tests further

--- edgeworth_test.go
+++ edgeworth_test.go
@@ -2,27 +2,28 @@ package edgeworth
 
 import "testing"
 
-func Test_GetGroup_1(t *testing.T) {
-	var control GroupBits
-	var bits ControlBits
-	var input Instruction
-	control = 0x3
-	input = 0x01234123
-	bits = input.GetControlBits()
+var DefaultInstructionEncoding Instruction = 0x01234123
 
+func Test_GetGroup_1(t *testing.T) {
+	control := GroupBits(0x3)
+	bits := DefaultInstructionEncoding.GetControlBits()
 	if bits.GetGroup() != control {
 		t.Errorf("Expected %d, got %d.", control, bits.GetGroup())
 	}
 }
 
 func Test_GetOperation_1(t *testing.T) {
-	var control OpBits
-	var bits ControlBits
-	var input Instruction
-	control = 0x15 // 0xA8 >> 3
-	input = 0x012341A8
-	bits = input.GetControlBits()
+	control := OpBits(0x4)
+	bits := DefaultInstructionEncoding.GetControlBits()
 	if bits.GetOperation() != control {
 		t.Errorf("Expected %d, got %d.", control, bits.GetOperation())
+	}
+}
+
+func Test_GetControlBits_1(t *testing.T) {
+	control := ControlBits(0x23)
+	bits := DefaultInstructionEncoding.GetControlBits()
+	if bits != control {
+		t.Errorf("Expected %d, got %d.", control, bits)
 	}
 }
