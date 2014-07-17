9b38a7fbc20969e49c7c8c3d7fc7330fccd50349
Joshua Scoggins <Theoretically.x64@gmail.com>
2014-07-16 20:19:03 -0700

    Updated tests further

--- edgeworth_test.go
+++ edgeworth_test.go
@@ -27,3 +27,40 @@ func Test_GetControlBits_1(t *testing.T) {
 		t.Errorf("Expected %d, got %d.", control, bits)
 	}
 }
+
+func Test_GetImmediate_1(t *testing.T) {
+	control := Word(0x0123)
+	bits := DefaultInstructionEncoding.GetImmediate()
+	if bits != control {
+		t.Errorf("Expected %d, got %d.", control, bits)
+	}
+}
+
+func Test_SetRegisterField_1(t *testing.T) {
+	var inst Instruction
+	inst = 0x23
+	err := inst.SetRegisterField(0, 0x41)
+	if err != nil {
+		t.Error(err)
+	}
+	err = inst.SetRegisterField(1, 0x23)
+	if err != nil {
+		t.Error(err)
+	}
+	err = inst.SetRegisterField(2, 0x01)
+	if err != nil {
+		t.Error(err)
+	}
+	if inst != DefaultInstructionEncoding {
+		t.Errorf("Expected %d, got %d.", DefaultInstructionEncoding, inst)
+	}
+}
+
+func Test_SetRegisterField_2(t *testing.T) {
+	var inst Instruction
+	inst = 0x23
+	err := inst.SetRegisterField(4, 0x41)
+	if err == nil {
+		t.Error("Attempt to set register field 4 succeded!")
+	}
+}
