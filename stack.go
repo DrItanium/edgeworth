804d19088ce4ba06a08f59d3225fbdb5ac107b35
Joshua Scoggins <Theoretically.x64@gmail.com>
2014-07-19 21:17:41 -0700

    Added more stack operations
    - Taken from factor, we now have swap and dup.
      - Swap replaces the top two elements of the stack
      - Dup pushes the top of the stack onto the stack

--- stack.go
+++ stack.go
@@ -14,6 +14,23 @@ func (c *Core) PopOffStack() Word {
 	return val
 }
 
+func (c *Core) PeekAtStack() Word {
+	return c.StackMemory[c.GetStackPointer()]
+}
+
 func (c *Core) InitStack() {
 	c.SetStackPointer(0xFFFF)
+}
+
+/* Swap the top two elements on the stack */
+func (c *Core) Swap2Stack() {
+	sp := c.GetStackPointer()
+	f := c.StackMemory[sp]
+	s := c.StackMemory[sp-1]
+	c.StackMemory[sp] = s
+	c.StackMemory[sp-1] = f
+}
+
+func (c *Core) DupStack() {
+	c.PushOntoStack(c.PeekAtStack())
 }
